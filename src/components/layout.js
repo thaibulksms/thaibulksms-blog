98def5170f628722d4921ab04e7d3d8a19053147
Phoowanon <phoowanon@1moby.com>
2019-06-04 16:51:25 +0700

    change css

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,16 @@
 import React from "react"
 // import { Link } from "gatsby"
 
-import { rhythm, scale } from "../utils/typography"
+import {
+  rhythm,
+  scale
+} from "../utils/typography"
 
 class Layout extends React.Component {
   render() {
-    const { children } = this.props
+    const {
+      children
+    } = this.props
     // const { location, title, children } = this.props
     // const rootPath = `${__PATH_PREFIX__}/`
     let header
@@ -13,37 +18,58 @@ class Layout extends React.Component {
 
     // }
 
-    header = (
-      <h1
-        style={{
+    header = ( <
+      h1 style = {
+        {
           ...scale(1.5),
           marginBottom: rhythm(1.5),
           marginTop: 0,
           textAlign: "center"
-        }}
-      >
-        <a href="https://www.thaibulksms.com/blog/" style={{
+        }
+      } >
+      <
+      a href = "https://www.thaibulksms.com/blog/"
+      style = {
+        {
           boxShadow: "none",
           display: "block"
-        }}><img src="/blog/images/logo.webp" alt="logo" /></a>
+        }
+      } >
+      <
+      img src = "/blog/images/logo.webp"
+      alt = "logo" / >
+      <
+      /a>
 
-        <hr className="hrLogo" />
-        {/* {title} */}
-      </h1 >
+      <
+      hr className = "hrLogo" / > {
+        /* {title} */ } <
+      /h1 >
     )
-    return (
-      <div>
-        <header>{header}</header>
-        <main className="main-width">{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, <a href={`https://www.thaibulksms.com`} style={{
-            boxShadow: "none"
-          }}>
-            ThaibulkSMS.com
-              </a>
-          {` `}
-        </footer>
-      </div>
+    return ( <
+      div >
+      <
+      header > {
+        header
+      } < /header> <
+      main className = "main-width" > {
+        children
+      } < /main> <
+      footer > ©{
+        new Date().getFullYear()
+      }, <
+      a href = {
+        `https://www.thaibulksms.com`
+      }
+      style = {
+        {
+          boxShadow: "none"
+        }
+      } >
+      ThaibulkSMS.com <
+      /a> <
+      /footer> <
+      /div>
     )
   }
 }
